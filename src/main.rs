d8aae8d2525c7314ac5be8a4a58e4ee2a262bbd0
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2016-10-08 11:25:15 +0200

    Backing up code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate byteorder;
-#[macro_use] extern crate log;
 extern crate env_logger;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate log;
+extern crate ncurses;
 
 mod processor;
 
 use std::process;
 use std::env::args;
+
+use ncurses::{printw};
 
 use processor::Processor;
 
@@ -15,6 +19,7 @@ fn main() {
         let mut p = Processor::new(path);
         process::exit(p.run())
     }
+    printw("Testing");
     println!("You must provide a ROM file.");
     process::exit(1);
 }
--- src/processor.rs
+++ src/processor.rs
@@ -1,17 +1,23 @@
 use std::fs::File;
 use std::io::{Cursor, Read};
 use std::path::Path;
+use std::time::Duration;
+use std::thread::sleep;
 
 use byteorder::{BigEndian, ReadBytesExt};
 use log::LogLevel::{Debug, Trace};
 
+lazy_static! {
+    pub static ref SLEEP_TIME: Duration = Duration::from_millis(1000 / 1);
+}
+
 pub struct Processor {
     registers: [u8; 16],
-    I: u16,
+    i: u16,
     delay: u8,
     sound: u8,
-    PC: u16,
-    SP: u8,
+    pc: u16,
+    sp: u8,
     rom: Vec<u8>,
 }
 
@@ -25,11 +31,11 @@ impl Processor {
         debug!("File size {} bytes", buf.len());
         Processor {
             registers: [0; 16],
-            I: 0,
+            i: 0,
             delay: 0,
             sound: 0,
-            PC: 200,
-            SP: 0,
+            pc: 200,
+            sp: 0,
             rom: buf,
         }
     }
@@ -42,17 +48,17 @@ impl Processor {
                 trace!("Memory {:?}", memory);
             }
 
-            cursor.set_position(self.PC as u64);
+            cursor.set_position(self.pc as u64);
             if log_enabled!(Debug) {
                 let mut rom = self.rom.clone();
-                let rest = rom.split_off(self.PC as usize);
+                let rest = rom.split_off(self.pc as usize);
                 let hex_rom: Vec<String> = rest.iter().map(|e| format!("{:x}", e)).collect();
                 debug!("Rest of the rom {:?} rom size {} {:x}",
                        hex_rom,
                        self.rom.len(),
                        self.rom.len());
             }
-            debug!("Cursor in {:x}", self.PC);
+            debug!("Cursor in {:x}", self.pc);
             let data = cursor.read_u8().unwrap();
             debug!("Data {:x}", data);
             let instruction = data >> 4;
@@ -60,32 +66,32 @@ impl Processor {
             match instruction {
                 0x1 => {
                     // TODO: Fix this
-                    cursor.set_position((self.PC) as u64);
+                    cursor.set_position((self.pc) as u64);
                     let addr = cursor.read_u16::<BigEndian>().unwrap();
                     debug!("Jumpping to {:x} on {:x}", (addr & 0xFFF) - 0x200, addr);
-                    self.PC = addr & 0xFFF - 0x200;
+                    self.pc = addr & 0xFFF - 0x200;
                 }
                 0x4 => {
                     if (data & 0xF) != cursor.read_u8().unwrap() {
-                        self.PC += 4
+                        self.pc += 4
                     } else {
-                        self.PC += 2
+                        self.pc += 2
                     }
                 }
                 0x5 => {
                     let x = data & 0xF;
                     let y = cursor.read_u8().unwrap() >> 4;
                     if self.registers[x as usize] == self.registers[y as usize] {
-                        self.PC += 4
+                        self.pc += 4
                     } else {
-                        self.PC += 2
+                        self.pc += 2
                     }
                 }
                 0x2 => {
                     // Call a function and increase stack.
-                    cursor.set_position((self.PC - 1) as u64);
-                    self.PC = cursor.read_u16::<BigEndian>().unwrap() & 0xFFF;
-                    self.SP += 1;
+                    cursor.set_position((self.pc - 1) as u64);
+                    self.pc = cursor.read_u16::<BigEndian>().unwrap() & 0xFFF;
+                    self.sp += 1;
                 }
                 0x6 => {
                     // Set value on register
@@ -93,7 +99,7 @@ impl Processor {
                     let value = cursor.read_u8().unwrap();
                     debug!("Setting {:x} on register {:x}", value, location);
                     self.registers[location] = value;
-                    self.PC += 2;
+                    self.pc += 2;
                 }
                 0x7 => {
                     // Increment value on register
@@ -101,7 +107,7 @@ impl Processor {
                     let location = (data & 0xF) as usize;
                     let value = cursor.read_u8().unwrap();
                     self.registers[location] += value;
-                    self.PC += 2;
+                    self.pc += 2;
                 }
                 0x8 => {
                     let subcommand = cursor.read_u8().unwrap() & 0xF;
@@ -122,19 +128,19 @@ impl Processor {
                         }
                         _ => panic!("Subcommand {:x} not found in command 0x8", subcommand),
                     };
-                    self.PC += 2
+                    self.pc += 2
                 }
                 0xA => {
                     // Set I value
-                    cursor.set_position((self.PC - 1) as u64);
+                    cursor.set_position((self.pc - 1) as u64);
                     let value = cursor.read_u16::<BigEndian>().unwrap() & 0xFFF;
                     debug!("Setting I to {:x}", value);
-                    self.I = value;
-                    self.PC += 2;
+                    self.i = value;
+                    self.pc += 2;
                 }
                 0xD => {
                     // Display sprite on screen
-                    self.PC += 2;
+                    self.pc += 2;
                 }
                 0xE => {
                     let value = data & 0xF;
@@ -142,24 +148,24 @@ impl Processor {
                     match subcommand {
                         0x9E => {
                             // If a key with value is pressed skip next
-                            // instruction PC += 4
+                            // instruction pc += 4
                             if false {
-                                self.PC += 2
+                                self.pc += 2
                             } else {
-                                self.PC += 4
+                                self.pc += 4
                             }
                         }
                         0xA1 => {
                             // If a key with value is not pressed skip next
-                            // instruction PC += 4
+                            // instruction pc += 4
                             if false {
-                                self.PC += 2
+                                self.pc += 2
                             } else {
-                                self.PC += 4
+                                self.pc += 4
                             }
                         }
                         unknown_code => {
-                            panic!("Unkown subcode {:x} not found on code {:x}",
+                            panic!("Unknown subcode {:x} not found on code {:x}",
                                    0xE,
                                    unknown_code);
                         }
@@ -171,7 +177,7 @@ impl Processor {
                     match subcommand {
                         0x07 => {
                             self.registers[(instruction & 0xF) as usize] = self.delay;
-                            self.PC += 2;
+                            self.pc += 2;
                         }
                         0x0A => {
                             // TODO: Wait for key
@@ -180,23 +186,27 @@ impl Processor {
                         }
                         0x15 => {
                             self.delay = self.registers[(instruction & 0xF) as usize];
-                            self.PC += 2;
+                            self.pc += 2;
 
                         }
                         0x18 => {
                             self.sound = self.registers[(instruction & 0xF) as usize];
-                            self.PC += 2
+                            self.pc += 2
                         }
                         0x1E => {
-                            self.I = self.I + (self.registers[(instruction & 0xF) as usize] as u16);
-                            self.PC += 2
+                            self.i = self.i + (self.registers[(instruction & 0xF) as usize] as u16);
+                            self.pc += 2
                         }
-                        0x29 => {}
-                        0x33 => {}
-                        0x55 => {}
+                        0x29 => {
+                            panic!("Not implemented yet")
+                        }
+                        0x33 => {
+                            panic!("Not implemented yet")}
+                        0x55 => {
+                            panic!("Not implemented yet")}
                         0x65 => {
                             // TODO: Really iterate over data and why?
-                            self.PC += 2
+                            self.pc += 2
                         }
                         unknown_code => {
                             panic!("Unkown subcode {:x} not found on code {:x}",
@@ -207,6 +217,7 @@ impl Processor {
                 }
                 unknown_code => panic!("Unkown code: {:x}", unknown_code),
             }
+             sleep(*SLEEP_TIME);
         }
     }
 }
